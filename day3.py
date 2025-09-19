743caf855d1c0c70dea6b8e9bdee2a88f8937e82
muh-younus <braq369@gmail.com>
2025-09-18 21:59:59 -0700

    start python

---
+++ day3.py
@@ -0,0 +1,6 @@
+list = ["Haleem", 12, "english", 3.5, True]
+print(list)
+print(type(list))
+print(len(list))
+list[0]="zia ul haq"
+print(list)
---
+++ demo.py
@@ -0,0 +1,76 @@
+# a = int(input("Enter First number"))
+# b = int(input("Enter second number"))
+
+# sum = a +b
+# print("The sum is " , sum)
+
+# a = float(input("Enter First number: "  ))
+# b = float(input("Enter second number: " ))
+# average = (a + b) / 2
+# print("The average is " , average)
+
+# c = int(input("Enter Third number:" ))
+# d = int(input("Enter Fourth number:" ))
+# print(c >= d)
+
+# str = "Muhammad"
+# length = len(str)
+# print(length)
+
+# Find the length of string 
+# str = input("Enter Your String")
+# length = len(str)
+# print(length)
+
+# str = "I am a string"
+# print(str.find("s"))
+
+# Traffic signal color is red , yellow and green
+
+# str = input("Enter the color of traffic signal")
+
+# if(str == "red"):
+#     print("stop")
+
+# elif(str == "yellow"):
+#     print("ready")
+
+# elif(str == "green"):
+#     print('go')
+
+# else:
+#     print("Signal is not exist that place")
+
+
+# mark = 75
+
+# if(mark >= 70 or mark < 80):
+#     print("A")
+
+# elif(mark >= 80 or mark < 90):
+#         print("B")
+# elif(mark >= 90 or mark < 100):
+#             print("A+")
+# else:
+#         print("Absent")
+
+# number = int(input("Enter the number"))
+
+# if(number % 7 == 0):
+#     print('The number is multiple of 7')
+# else:
+#     print("The number is not multiple of 7")
+
+# find the greatest of the three number entered by user
+
+x = int(input("Enter first number"))
+y = int(input("Enter second number"))
+z = int(input("Enter third number"))    
+
+if(x >= y and x >= z):
+    print("x is greatest")
+elif(y >= x and  y >= z):
+    print("y is greatest")
+else:
+    print("z is greatest")
+    
