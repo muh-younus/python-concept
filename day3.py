ff0ba13580b601cc6863359a201732f28b3779b7
muh-younus <braq369@gmail.com>
2025-09-20 00:28:02 -0700

    explore new concept

--- day3.py
+++ day3.py
@@ -1,6 +1,34 @@
-list = ["Haleem", 12, "english", 3.5, True]
-print(list)
-print(type(list))
-print(len(list))
-list[0]="zia ul haq"
-print(list)
+# list = ["Haleem", 12, "english", 3.5, True]
+# print(list)
+# print(type(list))
+# print(len(list))
+# list[0]="zia ul haq"
+# print(list)
+# subList = list[1:3]
+# print(subList)
+# list = [12,45,56,43,34]
+# list.append(100)
+
+# list.insert(2,200)
+# print(list)
+
+# # Practise Questions
+
+# movie1 = input("Enter your favourite movie name")
+# movie2 = input("Enter your second favourite movie name")
+# movie3 = input("Enter your third favourite movie name")
+
+# list =[]
+# list.append(movie1)
+# list.append(movie2)     
+# list.append(movie3) 
+# print(list)
+
+list = ["kick", "nuen", "level" "Zee"]
+str= list[0]
+
+if(str[0] == str[len(str)-1]):
+    print("The string is palindrome")
+else:
+    print("The string is not palindrome")
+
