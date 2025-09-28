94c7bd6a0ebd0a9ae1ac5e636f7875239216fa2c
muh-younus <braq369@gmail.com>
2025-09-28 01:08:04 -0700

    another commit

--- oops.py
+++ oops.py
@@ -32,48 +32,70 @@
 
 #blance , account number
 
-class hbl_account:
-    def __init__(self,balance):
-        self.accountBalance = balance
+# class hbl_account:
+#     def __init__(self,balance):
+#         self.accountBalance = balance
 
-    def credit(self,amount):
-        self.accountBalance += amount
-        print("You credit amount is",amount)
-        print(self.total_balance())
-    def debit(self,amount):
-        self.accountBalance -= amount
-        print("You credit amount is",amount)
-        print(self.total_balance())
+#     def credit(self,amount):
+#         self.accountBalance += amount
+#         print("You credit amount is",amount)
+#         print(self.total_balance())
+#     def debit(self,amount):
+#         self.accountBalance -= amount
+#         print("You credit amount is",amount)
+#         print(self.total_balance())
     
-    def total_balance(self):
-        print("Total balance is ",self.accountBalance)
+#     def total_balance(self):
+#         print("Total balance is ",self.accountBalance)
 
 
-account = hbl_account(2000)
-account.credit(1000)
-account.debit(300)
-account.credit(2000)
+# account = hbl_account(2000)
+# account.credit(1000)
+# account.debit(300)
+# account.credit(2000)
 
 
 
-class book:
-    def __init__(self,name,year):
-        self.book_name=name
-        self.publish=year
-    def old(self,name):
-        self.old_name=name
+# class book:
+#     def __init__(self,name,year):
+#         self.book_name=name
+#         self.publish=year
+#     def old(self,name):
+#         self.old_name=name
 
     
-branch = book("Atomic Habit",2020)
-print(branch.book_name)
-del branch.book_name
-branch.auther = "James Clear"
-print(branch.auther)
-branch2 = book("Atomic Habit",2020)
-# print(branch2.auther)
-print(branch2.book_name)
-print(branch2.publish)
+# branch = book("Atomic Habit",2020)
+# print(branch.book_name)
+# del branch.book_name
+# branch.auther = "James Clear"
+# print(branch.auther)
+# branch2 = book("Atomic Habit",2020)
+# # print(branch2.auther)
+# print(branch2.book_name)
+# print(branch2.publish)
 
-print(branch2.old("jackma"))
+# print(branch2.old("jackma"))
 
 
+#Inheritance
+
+# 
+
+class car:
+    def __init__ (self ,model,year):
+        self.model = model
+        self.year = year
+    @staticmethod
+    def drive():
+        print("The car is driving")
+
+class suzuki(car):
+    def __init__ (self,color,name,model,year):
+        self.color = color
+        self.name = name
+        super().__init__(model,year)
+        super().drive()
+
+company = suzuki("red","swift","silion",2020)
+print(company.model)
+print(company.year)
